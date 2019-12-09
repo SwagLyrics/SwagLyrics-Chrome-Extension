e8517c0072748c22a42d06add4507a7b05f1ac81
Frytek <flubbet@gmail.com>
2019-12-09 20:07:36 +0100

    Server based version working

--- notifications.js
+++ notifications.js
@@ -1,11 +1,10 @@
 var alert_str = "[SwagLyrics for Spotify]";
 console.log(alert_str, "initializing (available: " + (typeof window.Notification != 'undefined') + ")");
 var stay = 4000;
-var isFirst=true;
+var isFirst = true;
 var interval = 1000;
+var connectedInterval = 5000;
 var hash = hex_md5("temp");
-var current;
-var port = chrome.runtime.connect("mcdlgckdkknmfnnigdaekikebgkhhncj");
 const selectors = {
   albumArt:
     '#main .Root__now-playing-bar .now-playing-bar__left .cover-art-image.cover-art-image-loaded',
@@ -17,37 +16,88 @@ const selectors = {
     '#main .Root__now-playing-bar .now-playing-bar__center .player-controls__buttons button:nth-child(2)',
   nextBtn:
     '#main .Root__now-playing-bar .now-playing-bar__center .player-controls__buttons button:nth-child(4)',
+    playPauseBtnTitle:
+    '#main > div > div.Root__top-container > div.Root__now-playing-bar > footer > div > div.now-playing-bar__center > div > div.player-controls__buttons > div:nth-child(3) button'
 };
 var checks = {
-  art: function() {
+  art: function () {
     var $img = $('#cover-art').find('.sp-image-img');
     if ($img.length > 0) {
       return document.querySelector(`${selectors.albumArt}`).style.backgroundImage;
     }
     return null;
   },
-  name: function() {
+  name: function () {
     return document.querySelector(`${selectors.trackName}`).innerText;
   },
-  artist: function() {
+  artist: function () {
     return document.querySelector(`${selectors.artistName}`).innerText;
+  },
+  playState: function(){
+    return document.querySelector(`${selectors.playPauseBtnTitle}`).getAttribute("title");
   }
+  
 };
 
-port.onMessage.addListener(function(msg, sender, sendResponse) {
-  // When the background script asks for the current track-artist, send the data
-  console.log("Received", msg);
-  port.postMessage(current);
-});
+var testConnection = new XMLHttpRequest();
+var url = "http://127.0.0.1:5042/ping";
+openTestConnection();
+testServerConnection();
+
+function openTestConnection(){
+testConnection.open("GET", url, true);
+testConnection.setRequestHeader("Content-type", "application/json");
+testConnection.setRequestHeader("Access-Control-Allow-Origin", "*");
+testConnection.setRequestHeader("Access-Control-Allow-Headers", "*");
+}
 
 if (window.Notification) {
+  onWindowNotification();
+}
+
+function testServerConnection(){
+  var connectionInterval = setInterval(() => {
+    console.log("Testing connection...");
+    openTestConnection();
+    testConnection.send();
+    testConnection.onreadystatechange = function () {
+      if (testConnection.readyState == 4 && testConnection.status == 200) {
+        console.log("Connection established");
+        localStorage.scn_hash = hex_md5("");
+        onWindowNotification();
+        disconnectCheck();
+        clearInterval(connectionInterval);   
+      }
+    }
+  }, interval);
+}
+
+
+function disconnectCheck()
+{ 
+  var connectionInterval = setInterval(() => {
+  console.log("Testing connection...");
+  openTestConnection();
+  testConnection.send();
+  testConnection.onreadystatechange = function () {
+    if (testConnection.readyState == 4 && testConnection.status != 200) {
+      console.log("Disconnected");
+      testServerConnection();
+      clearInterval(connectionInterval);   
+    }
+  }
+}, interval);
+}
+
+
+function onWindowNotification() {
   console.log(alert_str, "requesting permission");
-  window.Notification.requestPermission(function() {
+  window.Notification.requestPermission(function () {
     console.log(alert_str, "permission granted");
     if (!localStorage.scn_hash) {
       localStorage.scn_hash = hash;
     }
-    setInterval(function() {
+    setInterval(function () {
       var result = {};
       var hash = null;
       var text = null;
@@ -61,38 +111,37 @@ if (window.Notification) {
           }
         }
       }
-xhr = new XMLHttpRequest();
-var url = "http://127.0.0.1:5042/getsong";
-xhr.open("POST", url, true);
-xhr.setRequestHeader("Content-type", "application/json");
-xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
-xhr.setRequestHeader("Access-Control-Allow-Headers","*");
-xhr.onreadystatechange = function () {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-        var json = JSON.parse(xhr.responseText);
-    }
-}
-var data = JSON.stringify({title:result.name,artist:result.artist});
-if(isFirst){
-  xhr.send(data);
-  isFirst=false;
-}
+
+      xhr = new XMLHttpRequest();
+      var url = "http://127.0.0.1:5042/getsong";
+      xhr.open("POST", url, true);
+      xhr.setRequestHeader("Content-type", "application/json");
+      xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+      xhr.setRequestHeader("Access-Control-Allow-Headers", "*");
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+          var json = JSON.parse(xhr.responseText);
+        }
+      }
+      var data = JSON.stringify({ title: result.name, artist: result.artist, playState: result.playState});
+      if (isFirst) {
+        xhr.send(data);
+        isFirst = false;
+      }
 
       if (cont) {
-        setTimeout(function() {
+        setTimeout(function () {
           hash = hex_md5(JSON.stringify(result));
-          current = result;
           if (localStorage.scn_hash !== hash) {
             localStorage.scn_hash = hash;
             console.log(alert_str, "new song", result);
-
 
             xhr.send(data);
             var notification = new window.Notification(result.name, {
               body: result.artist,
               icon: result.art
             });
-            setTimeout(function() {
+            setTimeout(function () {
               notification.close();
             }, stay);
           }
